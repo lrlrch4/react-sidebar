797022a6bd6b06b99ff3220c685997f6c987114a
lrlrch4 <luis.larosa.c@uni.pe>
2023-12-26 03:04:48 -0500

    Dark/light mode button added

---
+++ src/components/ThemeContext.js
@@ -0,0 +1,23 @@
+import { createContext, useContext, useState } from 'react';
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode((prevMode) => !prevMode);
+  };
+
+  const theme = isDarkMode ? 'dark' : 'light';
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => {
+  return useContext(ThemeContext);
+};
